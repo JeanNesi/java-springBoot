97bab1bcdc35a1353524e3ff20ce2abc8f2e5f96
Jean Carlos Nesi <jean.35322@alunosatc.edu.br>
2023-08-28 21:26:43 -0300

    first commit

---
+++ src/main/java/com/satc/satcloja/health/HealthCheckController.java
@@ -0,0 +1,11 @@
+package com.satc.satcloja.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {return "Ok";}
+}
